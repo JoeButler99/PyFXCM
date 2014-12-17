5069eb3db96a723b7e22df64c0c6a42ce6ebf65b
Joe Butler <joe_butler99@hotmail.com>
2014-12-17 23:19:45 +0000

    Outline for TestPythonModule completed

--- test/TestPythonModule.py
+++ test/TestPythonModule.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python
 import sys
 import unittest
-
+import yaml
+import re
 
 try:
     import PyFXCM
 except:
     print "Cannot import PyFXCM. Did you successfully build and install with 'make python-module; make install'?"
+    
+    
+class TestPyFXCM(unittest.TestCase):
+    def test_version(self):
+        version_str = PyFXCM.version()
+        simple_version_regex = re.compile("\d+[.]\d+[.]\d+")
+        self.assert_(simple_version_regex.match(version_str))
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
