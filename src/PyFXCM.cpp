4f88cc19752ad1d5bcb0df40561cf99927741959
Joe Butler <joe_butler99@hotmail.com>
2014-12-18 00:08:02 +0000

    Added the LoginParams class to the python module

--- src/PyFXCM.cpp
+++ src/PyFXCM.cpp
@@ -9,6 +9,13 @@
 #include "LoginParams.h"
 
 
+
+
+void set_login_params(std::string login, std::string password, std::string connection , std::string url) {
+
+}
+
+
 std::string version() {
 	return sem_ver_string;
 }
@@ -17,5 +24,12 @@ std::string version() {
 
 using namespace boost::python;
 BOOST_PYTHON_MODULE(PyFXCM) {
+	class_<LoginParams>("LoginParams", init<std::string, std::string, optional<std::string, std::string> >())
+		.def("get_login",&LoginParams::getLogin)
+		.def("get_password",&LoginParams::getPassword)
+		.def("get_url",&LoginParams::getURL)
+		.def("get_connection_type",&LoginParams::getConnection)
+	;
+
 	boost::python::def("version", version);
 }
--- test/TestPythonModule.py
+++ test/TestPythonModule.py
@@ -17,7 +17,22 @@ class TestPyFXCM(unittest.TestCase):
         self.assert_(simple_version_regex.match(version_str))
         
         
+class TestPyFXCMLoginParams(unittest.TestCase):
+    def test_constructor(self):
+        l1 = PyFXCM.LoginParams('user','test')
+        l2 = PyFXCM.LoginParams('user','test','Demo')
+        l3 = PyFXCM.LoginParams('user','test','Demo','www.google.co.uk')
+        for l in [l1,l2,l3]:
+            self.assertEqual(l.get_login(), 'user')
+            self.assertEqual(l.get_password(), 'test')
+        self.assertEqual(l1.get_connection_type(), "Real")
+        self.assertEqual(l2.get_connection_type(), "Demo")
+        self.assertEqual(l3.get_connection_type(), "Demo")
+        self.assertEqual(l1.get_url(), "http://www.fxcorporate.com/Hosts.jsp")
+        self.assertEqual(l2.get_url(), "http://www.fxcorporate.com/Hosts.jsp")
+        self.assertEqual(l3.get_url(), "www.google.co.uk")
         
+
         
         
 if __name__ == '__main__':
